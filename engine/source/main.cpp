def30f5386b036e53ff7693ee5ec604a1ff2a0f7
Spyro <epicdudeness677@gmail.com>
2023-01-16 00:54:47 +0100

    Implement a quick logger. Started working on getting some basic rendering done.

--- engine/source/main.cpp
+++ engine/source/main.cpp
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <iostream>
 #include <d3d11.h>
+#include "logging/Logger.h"
+#include <string>
+#include <typeinfo>
 
 void Engine::OnUpdate()
 {
@@ -10,11 +13,12 @@ void Engine::OnUpdate()
 void Engine::OnStart(StartContext& const someData)
 {
 	myGraphicsEngine.reset(new GraphicsEngine(someData.myWindowsInstance, someData.myWindowWidth, someData.myWindowHeight, 120));
+	LOG("Engine Initialized!");
 }
 
 void Engine::OnWinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	
+
 }
 
 extern "C"
